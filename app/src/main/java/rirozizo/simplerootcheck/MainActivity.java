d016d9a7f38c708323ca90d77a73ded592e96474
rirozizo <rawadzahreddine32@gmail.com>
2017-06-03 14:38:58 +0300

    Added Android Version Added Device name Less Hardcoded values Restructure code

--- app/src/main/java/rirozizo/simplerootcheck/MainActivity.java
+++ app/src/main/java/rirozizo/simplerootcheck/MainActivity.java
@@ -1,22 +1,25 @@
 package rirozizo.simplerootcheck;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.firebase.analytics.FirebaseAnalytics;
-
 public class MainActivity extends AppCompatActivity {
     TextView tv1;
+    TextView tv2;
+    TextView tv3;
     ImageView iv;
 
     private FirebaseAnalytics mFirebaseAnalytics;
@@ -29,8 +32,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initializing Strings
+        String line = "";
+        String root_check = "";
+        String version_check = "";
+        String device_check = "";
 
-        // Obtain the FirebaseAnalytics instance.
+        // Obtain the Firebase Analytics instance.
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
         // Google stuff... Ad related code
@@ -40,13 +48,13 @@ public class MainActivity extends AppCompatActivity {
 
         // Initializing Views
         tv1 = (TextView) findViewById(R.id.text1);
+        tv2 = (TextView) findViewById(R.id.textView);
+        tv3 = (TextView) findViewById(R.id.textView2);
         iv = (ImageView) findViewById(R.id.image);
 
         // Failure color by default because why not
         tv1.setTextColor(getResources().getColor(R.color.ROOT_TEXT_COLOR_NOPE));
 
-        String line = "";
-        String s = "";
         // Success value by default because I'm inconsistent
         boolean works = true;
         try {
@@ -67,7 +75,92 @@ public class MainActivity extends AppCompatActivity {
                     new BufferedReader(new InputStreamReader(stdout));
             while ((line = br.readLine()) != null) {
                 Log.d("[Output]", line);
-                s = s + line + "\n";
+                root_check = root_check + line + "\n";
+            }
+            br.close();
+
+            // If there's a problem with su
+            br =
+                    new BufferedReader(new InputStreamReader(stderr));
+            while ((line = br.readLine()) != null) {
+                Log.e("[Error]", line);
+                tv1.setText(R.string.root_problem);
+                works = false;
+            }
+            br.close();
+            process.waitFor();
+            process.destroy();
+
+        } catch (Exception ex) {
+            // If there's a general exception
+            tv1.setText(R.string.problem);
+        }
+
+        // More Google stuff
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "1");
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "root check");
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "check");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.APP_OPEN, bundle);
+
+        try {
+            // Executing the command su
+            Process process = Runtime.getRuntime().exec("su");
+
+            // Stream initialization
+            OutputStream stdin = process.getOutputStream();
+            InputStream stderr = process.getErrorStream();
+            InputStream stdout = process.getInputStream();
+
+            // If it worked fine
+            stdin.write(("getprop ro.build.version.release\n").getBytes());
+            stdin.write("exit\n".getBytes());
+            stdin.flush();
+            stdin.close();
+            BufferedReader br =
+                    new BufferedReader(new InputStreamReader(stdout));
+            while ((line = br.readLine()) != null) {
+                Log.d("[Output]", line);
+                version_check = version_check + line + "\n";
+            }
+            br.close();
+
+            // If there's a problem with su
+            br =
+                    new BufferedReader(new InputStreamReader(stderr));
+            while ((line = br.readLine()) != null) {
+                Log.e("[Error]", line);
+                tv1.setText(R.string.root_problem);
+                works = false;
+            }
+            br.close();
+            process.waitFor();
+            process.destroy();
+
+        } catch (Exception ex) {
+            // If there's a general exception
+            tv1.setText(R.string.problem);
+        }
+
+        try {
+            // Executing the command su
+            Process process = Runtime.getRuntime().exec("su");
+
+            // Stream initialization
+            OutputStream stdin = process.getOutputStream();
+            InputStream stderr = process.getErrorStream();
+            InputStream stdout = process.getInputStream();
+
+            // If it worked fine
+            stdin.write(("getprop ro.product.device\n").getBytes());
+            stdin.write("exit\n".getBytes());
+            stdin.flush();
+            stdin.close();
+            BufferedReader br =
+                    new BufferedReader(new InputStreamReader(stdout));
+            while ((line = br.readLine()) != null) {
+                Log.d("[Output]", line);
+                device_check = device_check + line + "\n";
             }
             br.close();
 
@@ -89,17 +182,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (works) {
+
             // WE HAVE ROOT!
             tv1.setTextColor(getResources().getColor(R.color.ROOT_TEXT_COLOR_WORKS));
-            tv1.setText(s);
+            tv1.setText(root_check);
+            iv.setImageResource(R.drawable.works);
+
+            // Dynamically changing resources in strings.xml to avoid hardcoded values as much as possible
+            Resources res = getResources();
+            String version = String.format(res.getString(R.string.version), version_check);
+            String device = String.format(res.getString(R.string.device), device_check);
+
+            tv2.setText(version);
+            iv.setImageResource(R.drawable.works);
+
+            tv3.setText(device);
             iv.setImageResource(R.drawable.works);
         }
-
-        // More Google stuff
-        Bundle bundle = new Bundle();
-        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "1");
-        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "root check");
-        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "check");
-        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.APP_OPEN, bundle);
     }
 }
