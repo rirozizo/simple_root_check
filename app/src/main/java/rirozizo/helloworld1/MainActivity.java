8aad16198ddd8b5cc47c9bc208c3bce698cf2747
rirozizo <rawadzahreddine32@gmail.com>
2017-09-08 17:00:24 +0300

    First Commit

---
+++ app/src/main/java/rirozizo/helloworld1/MainActivity.java
@@ -0,0 +1,24 @@
+package rirozizo.helloworld1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.textViewID);
+    }
+
+    public void changeText(View v)
+    {
+        tv.setText("How are you world");
+    }
+}
